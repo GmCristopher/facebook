b444b49b80cf1919555a94de5831bf517466fb00
Cristopher <cristophergrados2@hotmail.com>
2022-06-30 07:46:00 -0500

    Captura de la imagen con Droopzone

--- acciones_publicaciones.php
+++ acciones_publicaciones.php
@@ -1,20 +1,38 @@
-<?php
-require_once('publicaciones.php');
-$publicaciones=new publicaciones();
-$datos=$_REQUEST;
-$user=$datos['user'];
-$desc=$datos['desc'];
-$est=$datos['est'];
-$img=null;
-///guardo la publicacion
-$publicaciones->store($user,$desc,$est,$img);
-///busco el ultimo id de regisstrado
-$last=$publicaciones->last_id();
-//busco el registro completo 
-$registro=$publicaciones->show($last[0]['pub_id']);
+<?php 
+  require_once('Publicaciones.php');
+  $Publicaciones= new Publicaciones();
+  $datos=$_REQUEST;
 
-echo json_encode($registro);
+if(isset($_FILES['file'])){
 
+  $pub_id=$datos['aux_id'];      
+  $caracteres_permitidos = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $longitud = 8;
+  $name=substr(str_shuffle($caracteres_permitidos),0,$longitud);
+
+  $img= $_FILES['file'];
+  $ext = pathinfo($img['name'], PATHINFO_EXTENSION );// obtengo la extension del archivo
+  $fullname=$name.".".$ext;
+  move_uploaded_file($img["tmp_name"],"img/".$fullname);//
+  $Publicaciones->update_img($pub_id,$fullname);
+  echo $fullname;
+ 
+
+
+ }else{
+    
+  $user=$datos['user'];
+  $desc=$datos['desc'];
+  $estado=$datos['estado'];
+  $img=null;
+  //Guardo la publicacion
+  $Publicaciones->store($user,$desc,$estado,$img);
+  //Busco el último ID de registrado
+  $last=$Publicaciones->last_id();
+  //Busco el registro completo
+  $registro=$Publicaciones->show($last[0]['pub_id']);
+  echo json_encode($registro);
+ }   
 ?>
 
 
--- index.php
+++ index.php
@@ -1,98 +1,99 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Facebook</title>
-	<link rel="stylesheet" href="bootstrap.min.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>
-	<script>
-		$(document).on("click","#btn_publicar",()=>{
-          const user=$("#pub_usuario").val();
-          const desc=$("#pub_descripcion").val();
-          const est=$("#pub_estado").val();
-
-        $.ajax({
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" ></script>  
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" /> 
+    <script>
+     $(document).on("click","#btn_publicar",()=>{
+         const user=$("#pub_usuario").val();
+         const desc=$("#pub_descripcion").val();
+         const estado=$("#pub_estado").val();
+         $.ajax({
           url:'acciones_publicaciones.php',
-          data:{user:user,desc:desc,est:est},
+          data:{user:user,desc:desc,estado:estado},
           type:'POST',
           dataType:'json',
           success:(data)=>{
-          		console.log(data);
-
-          		$("#estado").text(data[0].pub_estado);
-          		$("#publicacion").text(data[0].pub_descripcion);
-          		if (data[0].pub_estado=='Alegre'){
-                        $(".cont_publicacion").removeClass("bg-primary");
-                        $(".cont_publicacion").removeClass("bg-warning");
-                        $(".cont_publicacion").addClass("bg-succes");
-                    }
-                    if (data[0].pub_estado=='Triste'){
-                        $(".cont_publicacion").removeClass("bg-warning");
-                        $(".cont_publicacion").removeClass("bg-succes");
-                        $(".cont_publicacion").addClass("bg-primary");
-                    }
-                    if (data[0].pub_estado=='Molesto'){
-                        $(".cont_publicacion").removeClass("bg-succes");
-                        $(".cont_publicacion").addClass("bg-primary");
-                        $(".cont_publicacion").addClass("bg-warning");
-                    }
-                 
-                           
+            console.log(data);
+                $("#aux_id").val(data[0].pub_id);
+                $("#estado").text(data[0].pub_estado);
+                $("#publicacion").text(data[0].pub_descripcion);
+                if(data[0].pub_estado=='Alegre'){
+                   $(".cont_publicacion").removeClass("bg-primary");
+                   $(".cont_publicacion").removeClass("bg-warning");
+                   $(".cont_publicacion").addClass("bg-success");
+                }
+                if(data[0].pub_estado=='Triste'){
+                   $(".cont_publicacion").removeClass("bg-success");
+                   $(".cont_publicacion").removeClass("bg-warning");
+                   $(".cont_publicacion").addClass("bg-primary");
+                }
+                if(data[0].pub_estado=='Molesto'){
+                   $(".cont_publicacion").removeClass("bg-success");
+                   $(".cont_publicacion").removeClass("bg-primary");
+                   $(".cont_publicacion").addClass("bg-warning");
+                }
           },error:(desc,estado)=>{
-          	//500 401 404 200
-          },
-            
-          })
-
-		});
-	</script>
+                //500 401 404 200
+                alert("Un error ha sucedido"+estado);
+            },
+         })
+     });
+    </script>
 </head>
 <body>
-	<h1 class="bg-success">VN-Facebook</h1>
-    
-    <div class="container">
-    	
-<div class="row">
-	<div class="col-md-6">
-		<input type="text" id="pub_usuario" placeholder="Usuario" class="form-control">
-    	<textarea id="pub_descripcion" rows="4" class="form-control"></textarea>
-    	<input type="file" id="pub_imagen " class="form-control">
-    	<select id="pub_estado" class="form-control bg-dark">
-    		<option value="">Elija una Opcion</option>
-    		<option value="Alegre">Alegre</option>
-    		<option value="Triste">Triste</option>
-    		<option value="Enojado">Enojado</option>
-    	</select>
-    	<div class="d-grid gap-2">
-    	   <button class="btn btn-primary" id="btn_publicar">Publicar</button>
+  <h1 class="bg-success">Publicaciones VN-Book</h1>
+  <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <input type="text" id="pub_usuario" placeholder="Usuario" class="form-control">
+          <textarea  id="pub_descripcion" rows="2" class="form-control"></textarea>
+          <select id="pub_estado" class="form-control bg-dark">
+            <option value="">Elija una Opcion</option>
+            <option value="Alegre">Alegre</option>
+            <option value="Triste">Triste</option>
+            <option value="Molesto">Molesto</option>
+          </select>
+          <div class="d-grid gap-2">
+            <button class="btn btn-primary" id="btn_publicar" >Publicar</button>
+          </div>
         </div>
-	</div>
-	<div class="col-md-6">
-      <img src="no_imagen.png" width="250px" alt="">
-	</div>
-</div>
-
-<div class="row">
-	<div class="col-md-4 ">
-    
-
-  <div class="card cont_publicacion" style="width: 11rem;">
-  	<img src="no-fhoto.jpg" class="card-img-top" alt="">
-  	<div class="card-body"> 
-  		<h3 id="estado">Estado</h3>
-  		<p class="card-text" id="publicacion">Descripcion</p>
-  			</div>
-  </div> 
-
-
-	</div>
-</div>
-    	
-
-
-
+        <div class="col-md-6">
+        <div class="col-md-3">
+          
+          <div id="dropz" class="dropzone"></div>
+        </div>       
+        </div>
+      </div>
+    <div class="row">
+        <div class="col-md-4">
+        <div class="card cont_publicacion"  style="width: 12rem;">
+              <img src="no_foto.jpg" class="card-img-top" id="aux_img" alt="...">
+              <div class="card-body">
+                    <h3 id="estado">Estado</h3>
+                <p class="card-text" id="publicacion" >Descripcion</p>
+            </div>
+        </div>
+        </div>
     </div>
-
+  </div>
 </body>
 </html>
+<script>
+var myDropzone = new Dropzone("#dropz", {
+    url: "acciones_publicaciones.php",
+         dictDefaultMessage: 'Arrastre el archivo aquí o haga clic para cargar',
+         paramName: "file",//
+    init: function () {
+        this.on("success", function (file, data) {
+            console.log(data);//
+            $("#aux_img").attr('src',`img/${data}`)
+        });//
+    }
+});
+</script>
